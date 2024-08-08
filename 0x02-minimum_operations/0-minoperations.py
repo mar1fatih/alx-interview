34f28670de323c345724d5331f338671fd4e75f8
mar1fatih <marouanefatih631@gmail.com>
2024-08-09 00:21:13 +0100

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""interview preparation"""
+
+
+def minOperations(n):
+    """minimum operations"""
+    if n <= 1 and type(n) is not int:
+        return 0
+    temp = n
+    pr = 2
+    list_sum = []
+    while pr <= temp:
+        if temp % pr == 0:
+            temp = temp / pr
+            if (isprime(pr)):
+                list_sum.append(pr)
+        else:
+            pr += 1
+    sum = 0
+    for i in list_sum:
+        sum += i
+    return sum
+
+def isprime(x):
+    """check if it a prime"""
+    j = 2
+    while j < x:
+        if x % j == 0:
+            return False
+        j += 1
+    return True
