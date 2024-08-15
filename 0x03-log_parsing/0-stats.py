5eab1479ab394952b8d0db117117054bb1fe4004
mar1fatih <marouanefatih631@gmail.com>
2024-08-15 16:34:34 +0100

    log parsing file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,35 @@
 #!/usr/bin/python3
-""" interviews tests """
+""" log parsing """
 import sys
-import re
-import signal
 
 
 i = 0
-file_size = 0
-list_codes = []
+files_size = 0
+codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+           '403': 0, '404': 0, '405': 0, '500': 0}
 
 
-def output(size, list_codes):
-    """ show the output """
-    codes = {}
-    for code in list_codes:
-        codes[code] = codes.get(code, 0) + 1
-
-    sorted_codes = {k: codes[k] for k in sorted(codes)}
-    print('File size:', size)
-    for k, v in sorted_codes.items():
-        print('{}: {}'.format(k, v))
+def output(total):
+    """ function to print """
+    print('File size: {}'.format(total))
+    for key, value in sorted(codes.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
 
 
-def signal_handler(sig, frame):
-    """ signal_handler Ctrl+C """
-    output(file_size, list_codes)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-for line in sys.stdin:
-    if i % 10 == 0 and i != 0:
-        output(file_size, list_codes)
-    pattern = r'^(?:\d{1,3}\.){3}\d{1,3} - \[\d{4}-\d{2}-\d{2} ' +\
-              r'.{15}\] \"GET /projects/260 HTTP/1\.1\" ' +\
-              r'(200|301|400|401|403|404|405|500) \d+$'
-    if re.match(pattern, line):
-        splited_code = line.split('"')[-1].split(' ')[1]
-        split_size = line.split('"')[-1].split(' ')[2][:-1]
-        list_codes.append(splited_code)
-        file_size += int(split_size)
-
-    i += 1
+try:
+    for line in sys.stdin:
+        split_line = line.split(" ")
+        if len(split_line) > 4:
+            code = split_line[-2]
+            if code in codes.keys():
+                codes[code] += 1
+            filesize = int(split_line[-1])
+            files_size += filesize
+            i += 1
+        if i % 10 == 0:
+            output(files_size)
+except Exception as e:
+    pass
+finally:
+    output(files_size)
