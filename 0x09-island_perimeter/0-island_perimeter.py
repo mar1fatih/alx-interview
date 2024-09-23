628982d6493a7a8d6dba7afef89ddc1e281a381d
mar1fatih <marouanefatih631@gmail.com>
2024-09-23 12:29:29 +0100

    grid island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""grid island"""
+def island_perimeter(grid):
+    c = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i - 1 >= 0:
+                    if grid[i - 1][j] == 0:
+                        c += 1
+                else:
+                    c += 1
+                if j + 1 < len(grid[0]):
+                    if grid[i][j + 1] == 0:
+                        c += 1
+                else:
+                    c += 1
+                if j - 1 >= 0:
+                    if grid[i][j - 1] == 0:
+                        c += 1
+                else:
+                    c += 1
+                if i + 1 < len(grid):
+                    if grid[i + 1][j] == 0:
+                        c += 1
+                else:
+                    c += 1
+    return c
